d62860a0dbc6b10f60d2e99869b42c934c1df9a1
Zhongming <atenliao@gmail.com>
2023-03-16 21:59:13 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(play='roller-skate'){
+    return `This Saturday, I want to ${play}!`;
+}
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`;
+}
+
+// wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%"
+
+function wrapAdjective(special='*') {
+        return function(str){
+            return `You are ${special}${str}${special}!`;
+        };
+};
